c99f8570b36a41543c0edf81bc758d8b645f2848
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-09-17 19:38:25 +0530

    Using Case  - Inserting Dummy Field in the Table

---
+++ CASE Queries/Invoice.sql
@@ -0,0 +1,17 @@
+--- Write a select query to return the 'InvoiceID' and a field called 'Status' from the invoices table. The status field 
+--- is either 'Late' or 'Paid' or 'Pending'. The status field will be 'Late' if the InvoiceDueDate is BEFORE 2016-04-30. 
+--- The status field will be 'Paid' if there is a value in the PaymentDate field. Lastly, if the invoice isn't late or paid, the status will be 'Pending'. Do not sort the results.
+
+SELECT InvoiceID,
+
+  CASE
+
+      WHEN InvoiceDueData <= '2016-04-30' THEN "Late"
+
+      WHEN PaymentDate != NULL THEN "Paid"
+      
+      ELSE "Pending"
+
+  END AS Status
+
+FROM invoices;
